903583258b7048dc1b332bcaa9ba63be7089e48e
boa20 <kwesikyeremateng5@gmail.com>
2024-10-07 23:50:33 +0000

    Updated the query samples code

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,12 +1,32 @@
-from relationship_app.models import *
+from relationship_app.models import Library, Librarian, Author, Book
 
 
-Library.objects.get(name="")
+library = Library.objects.create(name="Novels")
+library.save()
+
+librarian = Librarian.objects.create(name="Baning Adjei", library="Novels")
+librarian.save()
+
+author1 = Author.objects.create(name="George S. Clason")
+author1.save()
+author2 = Author.objects.create(name="Louisa May Alcott")
+author2.save()
+author3 = Author.objects.create(name="Charles Duhigg")
+author3.save()
+
+book1 = Book.objects.create(title="The Richest Man in Babylon", author= "George S. Clason")
+book1.save()
+book2 = Book.objects.create(title="Little Women", author= "Louisa May Alcott")
+book2.save()
+book3 = Book.objects.create(title="The Power of Habit", author= "Charles Duhigg")
+book3.save()
+
+Library.objects.get(name="Novels")
 books = Library.books.all()
 
 for book in books:
     print(book)
 
-booksByAuthor = Book.objects.filter(author="")
+booksByAuthor = Book.objects.filter(author="Charles Duhigg")
 
-librarian = Librarian.objects.filter(library="")
+librarian = Librarian.objects.filter(library="Novels")

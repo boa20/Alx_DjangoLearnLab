5e9ac51da4fe154b4198b4e56c45b4363991789a
boa20 <kwesikyeremateng5@gmail.com>
2024-10-07 23:58:48 +0000

    Updated the query samples code

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,3 +1,9 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+django.setup()
+
 from relationship_app.models import Library, Librarian, Author, Book
 
 
